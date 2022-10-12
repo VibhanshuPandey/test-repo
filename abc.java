e1400a4b7b7612eb52c41f8c44e85e93ea57d46d
VibhanshuPandey <111169133+VibhanshuPandey@users.noreply.github.com>
2022-10-12 13:23:10 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+
+public class abc{
+    public static void main(String args[]) {
+          Scanner in = new Scanner(System.in);
+          String s = in.nextLine();
+            string(s);
+            // System.out.println(s);
+    }
+
+    public static void string(String strtest) {
+        strtest += " World";
+        System.out.println(strtest);
+    }
+}
